63ebd74db22ae848ab7499b4018cc2710a7a58d9
DM1NIK <44864533+DM1NIK@users.noreply.github.com>
2018-11-16 20:30:48 +0100

    Add files via upload

---
+++ src/adressverwaltung/Adressverwaltung/src/adressverwaltung/Main.java
@@ -0,0 +1,15 @@
+package adressverwaltung;
+/**
+ *
+ * @author rehan.agarwal
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AdressverwaltungForm form = new AdressverwaltungForm();
+        form.setVisible(true);
+    }    
+}
